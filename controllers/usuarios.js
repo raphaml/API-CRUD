98ff786d32b5795719e84aea44fc97038e136a4f
raphael melo de lima <raphaelmelolima@gmail.com>
2018-02-06 10:14:44 -0200

    api crud

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,3 +1,5 @@
+const ObjectID = require('mongodb').ObjectID;
+
 // Função para listar todos os meus usuários que estão no banco de dados na coleção 'usuários'
 exports.listar = (req, res) => {
     req.db.collection('usuarios').find().toArray((err, result) => {
@@ -12,6 +14,41 @@ exports.listar = (req, res) => {
 
 // Função para testar o POST na minha rota '/api/usuarios/usuario'
 exports.criar = (req, res) => {
-    console.log('Processando requisição de um novo usuario.');
+    req.db.collection('usuarios').insert(req.body, (err, result) => {
+        if(err){
+            res.send(err);
+        }else{
+            res.sendStatus(201);
+        }
+    });
 }
+
+// FUNÇÃO PARA TESTAR O PUT NA MINHA ROTA /api/usuarios/usuario
+exports.atualizar = (req, res) => {
+    let id = req.params.idDaUrl
+
+    req.db.collection('usuarios').update({ "_id" :ObjectID(id)}, req.body, (err, result) =>{
+        if(err){
+            console.log(err);
+        }else{
+            res.sendStatus(201);
+        }
+    });
+}
+// FUNÇÃO PARA TESTAR O DELETE NA MINHA ROTA /api/usuarios/usuario
+exports.deletar = (req, res) =>{
+// DEPOIS CRIAMOS UMA VARIAVEL NA FUNCTION ONDE IREMOS ESPECIFICAR QUE O idDaUrl IRA PEGAR OS PARAMETROS
+// QUE PASSEI E ASSIM PROCURAR NO BANCO DE DADOS O ID CORRESPONDENTE
+    let id = req.params.idDaUrl
+
+    req.db.collection('usuarios').remove({"_id" :ObjectID(id)}, (err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.sendStatus(200);
+        }
+
+    });
+}
+
 
--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const usuariosController = require('./controllers/usuarios.js');
 // Instanciando o express
 const app = express();
 
-// Aplicando o body parser
+// Aplicando o body parser isso significa que tudo o que ele encontrar que nao for em formato json
+// ele ira automaticamente transformar no formato json
 app.use(bodyParser.json());
 
-// Conecta a nossa api ao banco de dados
-app.use(expressMongoDb('mongodb://localhost/estacaohack-turma2'));
+// Conecta a nossa api ao banco de dados usando a biblioteca do express-mongo-db
+app.use(expressMongoDb('mongodb://localhost/api-teste-projeto'));
 
 // Inicializando o servidor
 app.listen(3000, () => {
@@ -23,3 +24,9 @@ app.listen(3000, () => {
 // Endpoints
 app.get('/api/usuarios', usuariosController.listar);
 app.post('/api/usuarios/usuario', usuariosController.criar);
+
+// QUANDO EU USO O : idDaUrl ESTOU DIZENDO QUE MEUS METODOS HTTP put E delete
+// IRAO PEGAR OS PARAMETROS QUE EU COLOCAR DEPOIS DO /usuario
+// COMO SE FOSSE NOSSA query
+app.put('/api/usuarios/usuario/:idDaUrl', usuariosController.atualizar);
+app.delete('/api/usuarios/usuario/:idDaUrl', usuariosController.deletar);
